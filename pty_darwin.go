0f3ba43bfedbd07369114a1680f3638ad9a3eb10
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2013-11-10 09:24:37 -0500

    move to util, it's not darwin specific

--- pty_darwin.go
+++ pty_darwin.go
@@ -59,13 +59,6 @@ func unlockpt(f *os.File) error {
 	return ioctl(f.Fd(), syscall.TIOCPTYUNLK, 0)
 }
 
-func setsize(f *os.File, row uint16, cols uint16) error {
-  var ws winsize
-  ws.ws_col   = cols
-  ws.ws_row   = row
-	return ioctl(f.Fd(), syscall.TIOCSWINSZ, uintptr(unsafe.Pointer(&ws)))
-}
-
 func ioctl(fd, cmd, ptr uintptr) error {
 	_, _, e := syscall.Syscall(syscall.SYS_IOCTL, fd, cmd, ptr)
 	if e != 0 {
